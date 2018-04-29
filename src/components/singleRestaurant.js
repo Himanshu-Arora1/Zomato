c4f3f4df54a4259e3aa69a7b98455b40cac40203
Himanshu <himanshuarora1994@gmail.com>
2018-04-29 17:35:05 +0530

    Final Commit

--- src/components/singleRestaurant.js
+++ src/components/singleRestaurant.js
@@ -1,6 +1,10 @@
 import React,{Component} from 'react';
 import { fetchSingleRestaurant } from "../actions";
 import {connect} from 'react-redux';
+import {Pageheader} from 'react-bootstrap';
+
+
+// this refers to component instance
 
 class SingleRestaurant extends Component{
 
@@ -10,18 +14,34 @@ class SingleRestaurant extends Component{
         this.props.fetchSingleRestaurant(id);
     }
 
+    displayInfo(item){
+        return (
+        <div key={item.R.res_id}>
+             <div className="jumbotron heading2">
+                <h3> Zomato </h3>
+              </div>
+            <div class="heading2">
+              <h3> Restaurant name: <span> <h4 >  { item.name } </h4></span> </h3>
+              <h3> Address : <h4>{item.location.address} </h4></h3>
+            </div>
+            <div class="singleRestaurantImage">
+                <img src={item.featured_image} />
+            </div>
+        </div>
+        )
+    }
+
     render(){
         return(
             <div> 
-                <div>Himanshu </div>
-            <div> {this.props.singleRestaurant.name} </div>
+                 <div>{this.props.singleRestaurant.map((item) => this.displayInfo(item))}</div>
             </div>
         )
     }
 }
 
 function mapStateToProps({ singleRestaurant }) { 
-    console.log('single',singleRestaurant.name);
+    console.log('single',singleRestaurant);
      return { singleRestaurant };
 } 
 
